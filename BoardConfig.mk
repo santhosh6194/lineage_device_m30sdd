02518e0a95e47081bd109de990f4fbde5d1d8b05
santhosh6194 <santhosh6194@gmail.com>
2020-09-10 21:50:20 +0530

    m30sdd: added device configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,3 +16,28 @@
 
 DEVICE_PATH := device/samsung/m30sdd
 
+# Board
+TARGET_BOARD_PLATFORM := universal9611
+TARGET_SOC := exynos9611
+TARGET_BOOTLOADER_BOARD_NAME := exynos9611
+TARGET_BOARD_PLATFORM_GPU := mali-g72
+
+# build/make/core/Makefile
+TARGET_NO_BOOTLOADER := true
+
+# Enable hardware/samsung
+BOARD_VENDOR := samsung
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a73
+
+# Secondary Architecture
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
