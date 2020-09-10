e309df5247144f48f807762ba732a8eeb1de02b2
santhosh6194 <santhosh6194@gmail.com>
2020-09-10 22:19:10 +0530

    m30sdd: fix banner

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,4 @@
-opyright (C) 2020 The LineageOS Open Source Project
+# copyright (C) 2020 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
