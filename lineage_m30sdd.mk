77c9d5731c0189faae01acc6d212f3458975d936
santhosh6194 <santhosh6194@gmail.com>
2020-09-10 22:52:30 +0530

    m30sdd: added lineage_m30sdd.mk

---
+++ lineage_m30sdd.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier
+PRODUCT_NAME := lineage_m30sdd
+PRODUCT_DEVICE := m30sdd
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M307F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_PDA_MODEL := M307F
+PRODUCT_PDA_MODEL_VERSION := XXU3BTG1
+PRODUCT_PDA_VERSION := $(PRODUCT_PDA_MODEL)$(PRODUCT_PDA_MODEL_VERSION)
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+BUILD_FINGERPRINT := samsung/m30sdd/m30s:10/QP1A.190711.020/$(PRODUCT_PDA_VERSION):user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.PDA=$(PRODUCT_PDA_VERSION)
