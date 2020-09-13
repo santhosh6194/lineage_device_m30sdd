0f4e4b6d0f7934816e621c6cfe6e4321dadb967c
santhosh6194 <santhosh6194@gmail.com>
2020-09-13 23:06:18 +0530

    m30sdd: inherit device.mk

--- lineage_m30sdd.mk
+++ lineage_m30sdd.mk
@@ -20,6 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit device configuration
+$(call inherit-product, device/samsung/m30sdd/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
