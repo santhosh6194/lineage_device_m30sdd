6eb8365b92bcb9b65744ee3e8c9ba31dc30ccb6f
santhosh6194 <santhosh6194@gmail.com>
2020-09-14 00:14:42 +0530

    m30sdd: add config.fs

---
+++ config.fs
@@ -0,0 +1,17 @@
+[system/product/vendor_overlay/*/bin/*]
+mode: 0755
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[product/vendor_overlay/*/bin/*]
+mode: 0755
+user: AID_ROOT
+group: AID_SHELL
+caps: 0
+
+[efs/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_RADIO
+caps: 0
