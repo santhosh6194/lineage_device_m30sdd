0ba80cdd77832f3c273a0e0948508d8abbac563a
santhosh6194 <santhosh6194@gmail.com>
2020-09-13 23:03:33 +0530

    m30sdd: add PACKAGE->fastbootd

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio_keys.kl
