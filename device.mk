c7a737f5c8d8eaec55b04f9ad44d35c0ef53de2c
santhosh6194 <santhosh6194@gmail.com>
2020-09-13 22:48:46 +0530

    m30sdd: add PACKAGE->libvulkan

--- device.mk
+++ device.mk
@@ -42,3 +42,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio_keys.kl
 
+# SP-NDK
+PRODUCT_PACKAGES += \
+    libvulkan
