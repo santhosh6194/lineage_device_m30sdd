08ef1e35155f56faa7bc7095a546e7992e0bd90b
santhosh6194 <santhosh6194@gmail.com>
2020-09-13 22:51:22 +0530

    m30sdd: add PACKAGE->libsuspend

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio_keys.kl
 
+# LPM
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # SP-NDK
 PRODUCT_PACKAGES += \
     libvulkan
