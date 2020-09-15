d862d059bf5d89f1be19dd089a104c955efc10e0
santhosh6194 <santhosh6194@gmail.com>
2020-09-15 21:19:30 +0530

    m30sdd: added dtbo to build

---
+++ kernel/dtbo.mk
@@ -0,0 +1,15 @@
+MKDTIMG := $(HOST_OUT_EXECUTABLES)/mkdtimg$(HOST_EXECUTABLE_SUFFIX)
+
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
+DTBO_DIR   := $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/samsung
+
+DTBO_CFG := $(DEVICE_PATH)/kernel/$(TARGET_DEVICE).cfg
+
+define build-dtboimage-target
+    $(call pretty,"Target dtbo image: $(BOARD_PREBUILT_DTBOIMAGE)")
+    $(MKDTIMG) cfg_create $@ $(DTBO_CFG) -d $(DTBO_DIR)
+    $(hide) chmod a+r $@
+endef
+
+$(BOARD_PREBUILT_DTBOIMAGE): $(MKDTIMG) $(INSTALLED_KERNEL_TARGET)
+	$(build-dtboimage-target)
