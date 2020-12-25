83dc132c6d65ef98c6f836309ec8c063cc890731
santhosh6194 <santhosh6194@gmail.com>
2020-12-25 17:29:31 +0530

    m30sdd: Nuke FMRadio for now

--- device.mk
+++ device.mk
@@ -55,11 +55,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.sensor.proximity.xml
 
-# FM radio
-PRODUCT_PACKAGES += \
-    FMRadio \
-    libfmjni
-
 # Init
 PRODUCT_PACKAGES += \
     init.samsung.rc \
