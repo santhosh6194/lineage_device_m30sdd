6ea9225e949924725cfeae2064455bec8bb671e5
santhosh6194 <santhosh6194@gmail.com>
2021-01-15 03:04:14 +0530

    m30sdd: disable bluetooth wbs and minimize btcfg

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,25 +18,9 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#pragma push_macro("PROPERTY_VALUE_MAX")
+#define BTM_DEF_LOCAL_NAME "Samsung Galaxy M30s"
 
-#include <cutils/properties.h>
-#include <string.h>
-
-static inline const char* BtmGetDefaultName()
-{
-    return "Samsung Galaxy M30s";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
-
-#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED FALSE /* Don't prefer WBS    */
-
-#define BLE_VND_INCLUDED TRUE
-
-#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
-
-#pragma pop_macro("PROPERTY_VALUE_MAX")
+#define BLE_VND_INCLUDED    TRUE
+#define DISABLE_WBS TRUE
 
 #endif
